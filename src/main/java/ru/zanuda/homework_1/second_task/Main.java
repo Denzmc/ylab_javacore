8cf3acb5880c838679772225f9b4d76add995098
Denzmc <degtyar87@mail.ru>
2022-08-28 19:12:24 +0600

    task2

--- src/main/java/ru/zanuda/homework_1/second_task/Main.java
+++ src/main/java/ru/zanuda/homework_1/second_task/Main.java
@@ -1,4 +1,19 @@
 package ru.zanuda.homework_1.second_task;
 
 public class Main {
+    public static void main(String[] args) {
+        int array[] = {5,6,3,2,5,1,4,9};
+        int v = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[i]){
+                    v = array[i];
+                    array[i] = array[j];
+                    array[j] = v;
+                }
+            }
+            System.out.print(array[i] + " ");
+        }
+
+    }
 }
